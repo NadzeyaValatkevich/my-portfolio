d4273fa2066b19af33c14a39241814314bfaaa97
NadzeyaValatkevich <nadkonadzya@rambler.ru>
2023-04-28 23:36:51 +0300

    styled main and header blocks

--- src/main/Main.js
+++ src/main/Main.js
@@ -1,17 +1,21 @@
 import React from 'react';
-import style from './Main.module.css';
+import style from './Main.module.scss';
 import styleContainer from '../common/styles/Container.module.css'
 
 export const Main = () => {
     return (
         <div className={style.mainBlock}>
             <div className={styleContainer.container}>
-                <div className={style.text}>
-                    <span>Hi There</span>
-                    <h1>I am Nadzeya Valatkevich</h1>
-                    <p>Frontend Developer</p>
-                </div>
                 <div className={style.photo}></div>
+                <div className={style.text}>
+                    <h1 className={style.mainTitle}>Nadzeya Valatkevich</h1>
+                    <span className={style.line}></span>
+                    <p className={style.titleDescription}>Frontend Developer</p>
+                    <div className={style.btnConnection}>
+                        <a href={'#'}>Get in Touch</a>
+                    </div>
+
+                </div>
             </div>
 
         </div>
