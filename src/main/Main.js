9d886fdd2253f4900dc7faa20b9e410c19417a27
NadzeyaValatkevich <nadkonadzya@rambler.ru>
2023-05-02 23:28:49 +0300

    styled header and main, moved header to scss

--- src/main/Main.js
+++ src/main/Main.js
@@ -8,16 +8,14 @@ export const Main = () => {
             <div className={styleContainer.container}>
                 <div className={style.photo}></div>
                 <div className={style.text}>
-                    <h1 className={style.mainTitle}>Nadzeya Valatkevich</h1>
+                    <h1 className={style.mainTitle}>NADZEYA VALATKEVICH</h1>
                     <span className={style.line}></span>
                     <p className={style.titleDescription}>Frontend Developer</p>
                     <div className={style.btnConnection}>
                         <a href={'#'}>Get in Touch</a>
                     </div>
-
                 </div>
             </div>
-
         </div>
     );
 };
--- src/nav/Nav.js
+++ src/nav/Nav.js
@@ -3,12 +3,20 @@ import style from './Nav.module.scss'
 
 export const Nav = () => {
     return (
-        <div className={style.nav}>
-            <a href="">Main</a>
-            <a href="">Skills</a>
-            <a href="">Projects</a>
-            <a href="">Contacts</a>
+        <div className={style.navBlock}>
+            <div className={style.nav}>
+                <a href="">Main</a>
+                <a href="">Skills</a>
+                <a href="">Projects</a>
+                <a href="">Contacts</a>
+            </div>
+            <div className={style.hamburger}>
+                <div className={style.hamburgerBox}>
+                    <div className={style.hamburgerInner}></div>
+                </div>
+            </div>
         </div>
+
     );
 }
 
