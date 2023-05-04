315ca458a11a4698382a68115f559464606d2467
NadzeyaValatkevich <nadkonadzya@rambler.ru>
2023-05-04 23:49:00 +0300

    styled skills block and created component SvgGenerator

--- src/skills/skillsInners/SkillsInners.js
+++ src/skills/skillsInners/SkillsInners.js
@@ -1,4 +1,4 @@
-import style from './SkillsInners.css'
+import style from './SkillsInners.module.scss'
 import {Skill} from "./skill/Skill";
 export const SkillsInners = () => {
     return (
@@ -7,7 +7,7 @@ export const SkillsInners = () => {
                 <h3 className={style.nameSkill}>My tech Skills</h3>
             </div>
 
-            <p className={style.skillsText}>mknnnn njjmmm mkjnnhhuu mmmkkkjjj</p>
+            <p className={style.skillsText}>I have tried out lots of different technologies and am always open to new things. Here are the ones I have got most experience with:</p>
             <div className={style.skillsInnersBlock}>
                 <Skill></Skill>
                 <Skill></Skill>
--- src/skills/skillsInners/skill/Skill.js
+++ src/skills/skillsInners/skill/Skill.js
@@ -1,10 +1,13 @@
 import React from 'react';
-import style from './Skill.module.css'
+import style from './Skill.module.scss'
+import {SvgGenerator} from "../../../common/components/svgGenerator/SvgGenerator";
 
 export const Skill = (props) => {
     return (
         <div className={style.skill}>
-            <span className={style.skillIcon}></span>
+            <span className={style.skillIcon}>
+                <SvgGenerator title={props.title}></SvgGenerator>
+            </span>
             <div className={style.skillBlock}>
                 <div className={style.skillTitle}>
                     <h3>{props.title}</h3>
